b50a39d08fa4d4a522d6cb73ba26334efaaf5c2a
manojchauhan33 <manojchauhan9963@gamil.com>
2025-07-28 14:41:04 +0530

    add some features

---
+++ public/js/admin.js
@@ -0,0 +1,10 @@
+  function goToAssignPage() {
+    const teacherId = document.getElementById('teacherSelect').value;
+
+    if (teacherId) {
+      window.location.href = `/assign-timetable?teacherId=${teacherId}`;
+    } else {
+      alert('Please select a teacher.');
+    }
+    
+  }
